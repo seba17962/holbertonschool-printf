1bdc6d391cab0c57fa422d296b934bdb4be0acc7
seba17962 <isebsebastiansilva@gmail.com>
2023-07-17 07:09:29 -0700

    all test

--- test.c
+++ test.c
@@ -9,19 +9,27 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
+	int len;
+	int len2;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
-    
-	(void)len;
-	(void)len2;
-	return (0);
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	putchar('\n');
+	putchar('\n');
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	putchar('\n');
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+
+return (0);
 }
